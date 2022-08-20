9b6ef72698ba53f1d7809b971b2fa1efac2862db
Arnaldo Neto <vilela-neto@hotmail.com>
2022-08-19 23:29:47 -0300

    46. Implementando o cenário onde o livro não existe no servidor

--- src/test/java/com/arnaldoneto/libraryapi/api/resource/BookControllerTest.java
+++ src/test/java/com/arnaldoneto/libraryapi/api/resource/BookControllerTest.java
@@ -139,6 +139,22 @@ public class BookControllerTest {
 
     }
 
+    @Test
+    @DisplayName("Deve retornar resource not found quando o livro procurado não existir")
+    public void bookNotFoundTest() throws Exception{
+
+        BDDMockito.given(service.getById(Mockito.anyLong())).willReturn(Optional.empty());
+
+        // execução (when)
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .get(BOOK_API.concat("/" + 1))
+                .accept(MediaType.APPLICATION_JSON);
+
+        mvc
+            .perform(request)
+            .andExpect(status().isNotFound());
+    }
+
     private BookDTO createNewBook() {
         return BookDTO.builder().author("Arthur").title("As aventuras").isbn("001").build();
     }
