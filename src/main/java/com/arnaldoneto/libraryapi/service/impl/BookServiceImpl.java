40b6f3e9f0effd618efdd56eb2f0db262e64d275
Arnaldo Neto <vilela-neto@hotmail.com>
2022-08-21 13:33:39 -0300

    49. Teste unitário de obter livro por id na camada de serviço

--- src/main/java/com/arnaldoneto/libraryapi/service/impl/BookServiceImpl.java
+++ src/main/java/com/arnaldoneto/libraryapi/service/impl/BookServiceImpl.java
@@ -26,7 +26,7 @@ public class BookServiceImpl implements com.arnaldoneto.libraryapi.service.BookS
 
     @Override
     public Optional<Book> getById(Long id) {
-        return Optional.empty();
+        return this.repository.findById(id);
     }
 
     @Override
